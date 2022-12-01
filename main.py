61c2ff9cc0b5c5a1cafbdd8520506dca817ff1fb
fl1ckthekid <bogdan-1107@yandex.ru>
2022-12-01 22:38:18 +0300

    g

---
+++ main.py
@@ -0,0 +1,19 @@
+def calcul(expression):
+    allowed = '+-/*'
+    for sign in allowed:
+        if sign in expression:
+            try:
+                a, b = expression.split(sign)
+                a, b = int(a), int(b)
+                if sign == '+':
+                    return a + b
+                if sign == '-':
+                    return a - b
+                if sign == '*':
+                    return a * b
+                if sign == '/':
+                    return a / b
+            except(ValueError, TypeError):
+                raise ValueError('не корректный ввод')
+if __name__ == '__main__':
+    calcul('')
